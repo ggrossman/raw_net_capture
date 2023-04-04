6ec8b557763ff6417ebb865724843fdfed4fec05
Alex Maestas <alex.maestas@zendesk.com>
2023-04-04 12:20:30 -0700

    patch Net::BufferedIO more hygenically
    this captures debug output by calling the original method without raw_net_capture in place, and then performs our actions.

--- lib/raw_net_capture/net/buffered_io.rb
+++ lib/raw_net_capture/net/buffered_io.rb
@@ -3,25 +3,42 @@
 module Net
   class BufferedIO
     private
+    alias_method :orig_rbuf_consume, :rbuf_consume
+    alias_method :orig_write0, :write0
 
-    def rbuf_consume(len)
-      str = @rbuf.slice!(0, len)
-      if @debug_output
-        @debug_output << %Q[-> #{str.dump}\n]
-        @debug_output.received(str) if @debug_output.respond_to?(:received)
+    def rbuf_consume(len = nil)
+      return orig_rbuf_consume(len) unless @debug_output
+
+      begin
+        saved_debug_output = @debug_output
+        @debug_output = nil
+        str = orig_rbuf_consume(len)
+      ensure
+        @debug_output = saved_debug_output
       end
+
+      @debug_output << %Q[-> #{str.dump}\n]
+      @debug_output.received(str) if @debug_output.respond_to?(:received)
       str
     end
 
-    def write0(str)
-      if @debug_output
+    def write0(*strs)
+      return orig_write0(*strs) unless @debug_output
+
+      begin
+        saved_debug_output = @debug_output
+        @debug_output = nil
+        ret = orig_write0(*strs)
+      ensure
+        @debug_output = saved_debug_output
+      end
+
+      strs.each do |str|
         @debug_output << str.dump
         @debug_output.sent(str) if @debug_output.respond_to?(:sent)
       end
 
-      len = @io.write(str)
-      @written_bytes += len
-      len
+      ret
     end
   end
 end
